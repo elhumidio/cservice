736ff2059157ac3fdbe97b4a0418ca4522ded362
Damian Blanc <damian.blanc@turijobs.com>
2022-08-09 17:41:49 +0200

    Change configurationsection by configuration

--- src/Infraestructure/Integrations/GeoNamesConector.cs
+++ src/Infraestructure/Integrations/GeoNamesConector.cs
@@ -8,24 +8,21 @@ namespace Infraestructure.Integrations
 {
     public class GeoNamesConector : IGeoNamesConector
     {
-        private IConfigurationSection _geoNameConfiguration;
-        private string _geoNameUrl;
+        private string _geoNamesUrl;
 
-        public GeoNamesConector()
+        public GeoNamesConector(IConfiguration configuration)
         {
+            _geoNamesUrl = configuration["GeoNames:BaseUrl"].ToString();
 
-            _geoNameConfiguration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("GeoNames");
-            _geoNameUrl = _geoNameConfiguration.GetValue<string>("BaseUrl");
         }
         public GeoNamesDTO GetPostalCodesCollection(string postalCode, string country)
         {
-            var geoNames = new GeoNamesDTO();
-            string url = $"{_geoNameUrl}?postalcode={postalCode}&country={country}&maxRows=100&username=turijobs";
+            GeoNamesDTO geoNames = new();
+            string url = $"{_geoNamesUrl}?postalcode={postalCode}&country={country}&maxRows=100&username=turijobs";
 
             using (var httpClient = new HttpClient())
             {
                 geoNames = JsonConvert.DeserializeObject<GeoNamesDTO>(httpClient.GetStringAsync(new Uri(url)).Result);
-
             }
 
             return geoNames;
