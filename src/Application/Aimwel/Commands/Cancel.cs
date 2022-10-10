9383f32d5cfebc24d47216544d4cd45c0ad9a1a3
STEPSTONE-ASA\blancd01 <blancd01@stepstone.com>
2022-10-10 09:58:25 +0200

    ...

--- src/Application/Aimwel/Commands/Cancel.cs
+++ src/Application/Aimwel/Commands/Cancel.cs
@@ -1,12 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Application.Aimwel.Interfaces;
+using Application.Core;
+using MediatR;
 
 namespace Application.Aimwel.Commands
 {
     public class Cancel
     {
+        public class Command : IRequest<Result<bool>>
+        {
+            public int offerId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<bool>>
+        {
+            private readonly IAimwelCampaign _manageCampaign;
+
+            public Handler(
+                IAimwelCampaign aimwelCampaign)
+            {
+                _manageCampaign = aimwelCampaign;
+            }
+
+            public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var response = await _manageCampaign.StopCampaign(request.offerId);
+                return Result<bool>.Success(response);
+            }
+        }
     }
 }
--- src/Application/Aimwel/Commands/Create.cs
+++ src/Application/Aimwel/Commands/Create.cs
@@ -1,12 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Application.Aimwel.Interfaces;
+using Application.Core;
+using Domain.Repositories;
+using DPGRecruitmentCampaignClient;
+using MediatR;
 
 namespace Application.Aimwel.Commands
 {
-    internal class Create
+    public class Create
     {
+        public class Command : IRequest<Result<CreateCampaignResponse>>
+        {
+            public int offerId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<CreateCampaignResponse>>
+        {
+            private readonly IJobOfferRepository _offerRepo;
+
+            private readonly IAimwelCampaign _manageCampaign;
+
+            public Handler(IJobOfferRepository offerRepo,
+                IAimwelCampaign aimwelCampaign)
+            {
+                _offerRepo = offerRepo;
+                _manageCampaign = aimwelCampaign;
+            }
+
+            public async Task<Result<CreateCampaignResponse>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var job = _offerRepo.GetOfferById(request.offerId);
+                if (job == null) return null;
+                else
+                {
+                    var campaign = await _manageCampaign.CreateCampaing(job);
+                    return Result<CreateCampaignResponse>.Success(campaign);
+                }
+            }
+        }
     }
 }
