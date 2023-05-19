0bdaf14efe6430ffe8525e215e526019f086208b
Gonzalo Rodriguez Benitez <rodrig01@stepstone.com>
2023-05-19 09:50:08 +0200

    Contracts nuget

---
+++ test/TURI.ContractService.Contracts.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TURI.ContractService.Contracts.Tests.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
