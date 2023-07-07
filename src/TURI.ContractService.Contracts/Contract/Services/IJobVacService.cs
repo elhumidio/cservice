d7926471ff1dcf43dafd951a8b4b0cc328d0509f
Sky Moynihan <sky.moynihan@turijobs.com>
2023-07-07 13:22:12 +0200

    Adjusted permissions for IJobVacService

--- src/TURI.ContractService.Contracts/Contract/Services/IJobVacService.cs
+++ src/TURI.ContractService.Contracts/Contract/Services/IJobVacService.cs
@@ -2,7 +2,7 @@ using Refit;
 
 namespace TURI.ContractService.Contracts.Contract.Services
 {
-    internal interface IJobVacService
+    public interface IJobVacService
     {
         [Get("/api/JobMatchingController/GetExternalJobId")]
         Task<string> GetExternalJobId(int jobId);
