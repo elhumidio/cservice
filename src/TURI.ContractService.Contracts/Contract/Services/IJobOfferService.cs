d5f5476454ad66a217a949d03159230163a99636
Sky Moynihan <sky.moynihan@turijobs.com>
2023-09-19 17:41:48 +0200

    Added contract for getting ATS Jobs info from Enterprise and ATS

--- src/TURI.ContractService.Contracts/Contract/Services/IJobOfferService.cs
+++ src/TURI.ContractService.Contracts/Contract/Services/IJobOfferService.cs
@@ -1,5 +1,6 @@
 using Refit;
 using TURI.ContractService.Contract.Models;
+using TURI.ContractService.Contracts.Contract.Models;
 using TURI.ContractService.Contracts.Contract.Models.Requests;
 
 namespace TURI.ContractService.Contracts.Contract.Services;
@@ -14,4 +15,7 @@ public interface IJobOfferService
     
     [Post("/api/JobOffer/GetOffersForView")]
     Task<OfferInfoMinForViewResponse[]> GetOffersForView(OfferInfoRequest request);
+
+    [Post("/api/JobOffer/ListOffersAtsInfo")]
+    Task<List<OfferMinInfoAts>> ListOffersAtsInfo(OfferMinInfoAtsRequest request);
 }
