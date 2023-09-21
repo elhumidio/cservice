3c968356e16a925527a8e819163afd731bd7ccca
STEPSTONE-ASA\KleinE02 <Estefania Klein>
2023-09-21 16:01:12 +0200

    Extensions not in use anymore. Changed data context salarymin and salarymax types to decimal.

--- src/Application/Utils/Extensions.cs
+++ src/Application/Utils/Extensions.cs
@@ -2,9 +2,9 @@ namespace Application.Utils
 {
     public static class Extensions
     {
-        public static int NumberString(this Enum enVal)
-        {
-            return enVal;
-        }
+        //public static int NumberString(this Enum enVal)
+        //{
+        //    return enVal;
+        //}
     }
 }
