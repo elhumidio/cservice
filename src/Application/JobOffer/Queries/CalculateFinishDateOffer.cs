a73bcd0a4da068a1c042284320697676961937c1
Damian Blanc <blancd01@stepstone.com>
2024-02-23 13:12:48 +0100

    Cambio calculo de fecha en las ofertas basado en la duracion del producto

--- src/Application/JobOffer/Queries/CalculateFinishDateOffer.cs
+++ src/Application/JobOffer/Queries/CalculateFinishDateOffer.cs
@@ -29,33 +29,10 @@ namespace Application.JobOffer.Queries
             }
 
             public async Task<Result<DateTime>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                // la mayor de las tres
-                var contract = _contractRepo.Get(request.ContractID).First();
-                var contractFinishDate = contract != null ? contract.FinishDate : DateTime.Now;
+            {                   
                 var prodDuration = _productRepo.GetProductDuration(request.ProductId);
-                var prodBasedFinishDate = DateTime.Now.AddDays(prodDuration);
-                var prodLineDuration = _productLineRepo.GetProductLineDuration(request.ProductId);
-                var prodLineBasedFinishDate = DateTime.Now.AddDays(prodLineDuration);
-                DateTime firstCompareWinner;
-                DateTime secondCompareWinner;
-                if (contractFinishDate.Value.Date.CompareTo(prodBasedFinishDate.Date) < 1)
-                {
-                    firstCompareWinner = contractFinishDate.Value.Date;
-                }
-                else
-                {
-                    firstCompareWinner = prodBasedFinishDate.Date;
-                }
-                if (firstCompareWinner.Date.CompareTo(prodLineBasedFinishDate) < 1)
-                {
-                    secondCompareWinner = firstCompareWinner;
-                }
-                else
-                {
-                    secondCompareWinner = prodLineBasedFinishDate.Date;
-                }
-                return await Task.FromResult(Result<DateTime>.Success(secondCompareWinner));
+                var finishDate = DateTime.Now.AddDays(prodDuration);                
+                return await Task.FromResult(Result<DateTime>.Success(finishDate));
             }
         }
     }
