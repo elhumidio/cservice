5e5b2fe3fb6870b1995c8310b9215db4f531adaf
Damian Blanc <blancd01@stepstone.com>
2024-03-01 17:42:10 +0100

    Added method getpayment

--- src/Application/Contracts/Queries/List.cs
+++ src/Application/Contracts/Queries/List.cs
@@ -2,6 +2,7 @@ using Application.Contracts.DTO;
 using Application.Core;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Domain.Entities;
 using Domain.Repositories;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +31,33 @@ namespace Application.Contracts.Queries
             {
                 var query = _contractRepo.GetContracts(request.CompanyId).ProjectTo<ContractDto>(_mapper.ConfigurationProvider).AsQueryable();
                 return Result<List<ContractDto>>.Success(await query.ToListAsync());
+            }
+        }
+    }
+
+
+    public class ListPayments
+    {
+        public class Query : IRequest<Result<ContractPayment>>
+        {
+            public int CompanyId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<ContractPayment>>
+        {
+            private readonly IContractRepository _contractRepo;
+            private readonly IMapper _mapper;
+
+            public Handler(IMapper mapper, IContractRepository contractRepo)
+            {
+                _mapper = mapper;
+                _contractRepo = contractRepo;
+            }
+
+            public async Task<Result<ContractPayment>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var query = _contractRepo.GetContracts(request.CompanyId).ProjectTo<ContractDto>(_mapper.ConfigurationProvider).AsQueryable();
+                return Result<ContractPayment>.Success(await query.ToListAsync());
             }
         }
     }
